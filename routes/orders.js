8b25b306082bd0a1ec0592129ef49a517fb8798c
Dmitrii <demyanoff1337@yandex.ru>
2022-02-25 00:07:17 +0300

    test html

--- routes/orders.js
+++ routes/orders.js
@@ -1,11 +1,22 @@
 const express = require('express');
-const { User, Product, Order, Role } = require('../db/models');
+const {
+  User,
+  Product,
+  Order,
+  Role
+} = require('../db/models');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const products = await Product.findAll();
-  res.render('orders', { products });
+  const products = await Product.findAll({
+    where: {
+      status: 'placed'
+    }
+  });
+  res.render('orders', {
+    products
+  });
 });
 
 module.exports = router;
